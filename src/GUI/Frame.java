c181b99ebb8f023ddd12516a1eda73c4a0f15590
Valerio <misterx.valerio@hotmail.com>
2017-03-02 23:35:43 +0100

    Creata una classe per creare un frame vuoto

--- src/GUI/Frame.java
+++ src/GUI/Frame.java
@@ -1,5 +1,15 @@
 package GUI;
 
-public class Frame {
+import java.awt.*;
+import javax.swing.*;
 
+public class Frame {
+	private static void newframe(){ 			/*Metodo per creare un frame base*/
+		JFrame frame = new JFrame("Autonoleggio");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JLabel label = new JLabel();
+		frame.getContentPane().add(label, BorderLayout.CENTER);
+		frame.pack();
+		frame.setVisible(true);
+	}
 }
