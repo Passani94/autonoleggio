34dfdb072db7e9d66b10f106d57014b8ace6ebf9
Abba29 <lory369@gmail.com>
2017-03-02 12:16:53 +0100

    Prova per Savio

--- src/autonoleggio/Autonoleggio.java
+++ src/autonoleggio/Autonoleggio.java
@@ -3,7 +3,8 @@ package autonoleggio;
 public class Autonoleggio {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World");
+		System.out.println("Hello, World");	
+		System.out.println("Hello, Hello");
 
 	}
 
