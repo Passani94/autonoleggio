623ae6746b19b4b0db8e32fef0a5fc96317c6338
Valerio <misterx.valerio@hotmail.com>
2017-03-09 13:01:25 +0100

    Aggiunto Calendario(non completo)

--- src/GUI/Admin/Pannello.java
+++ src/GUI/Admin/Pannello.java
@@ -8,22 +8,20 @@ import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
 import java.awt.Font;
-import javax.swing.JFormattedTextField;
-import java.awt.Color;
 
-import javax.swing.SwingConstants;
 import javax.swing.LayoutStyle.ComponentPlacement;
 
-import GUI.Login;
+import GUI.*;
+import javax.swing.JLabel;
 
 public class Pannello extends JFrame implements ActionListener{
 
 	private JPanel contentPane;
 	private JButton btnEsci = new JButton("Esci");
 	private JButton btnLogout = new JButton("Logout");
+	private Calendario cal = new Calendario();
+	
 	/* Crea il frame Login.*/
 	
 	public void run() {
@@ -44,29 +42,18 @@ public class Pannello extends JFrame implements ActionListener{
 		setBounds(100, 100, 800, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);/* Action Listener per il bottone Accedi.*/
-	    
-		btnEsci.setFont(new Font("Arial", Font.PLAIN, 12));
-		btnEsci.addActionListener(this);
+		setContentPane(contentPane);
 		
-		JFormattedTextField frmtdtxtlog = new JFormattedTextField();
-		frmtdtxtlog.setText("Loggato come");
-		frmtdtxtlog.setHorizontalAlignment(SwingConstants.CENTER);
-		frmtdtxtlog.setBorder(null);
-		frmtdtxtlog.setForeground(new Color(0, 0, 0));
-		frmtdtxtlog.setFont(new Font("Arial", Font.PLAIN, 12));
-		frmtdtxtlog.setEditable(false);
+		btnEsci.setFont(new Font("Arial", Font.PLAIN, 12));
+		btnEsci.addActionListener(this); /* Action Listener per il bottone Esci.*/
 		
 		
 		btnLogout.setFont(new Font("Arial", Font.PLAIN, 12));
-		btnLogout.addActionListener(this);
+		btnLogout.addActionListener(this); /* Action Listener per il bottone Logout.*/
 		
-		JFormattedTextField frmtdtxtuser = new JFormattedTextField();
-		frmtdtxtuser.setHorizontalAlignment(SwingConstants.CENTER);
-		frmtdtxtuser.setForeground(Color.BLACK);
-		frmtdtxtuser.setFont(new Font("Arial", Font.PLAIN, 12));
-		frmtdtxtuser.setEditable(false);
-		frmtdtxtuser.setBorder(null);
+		JLabel lbllog = new JLabel("Loggato come");
+		lbllog.setFont(new Font("Arial", Font.PLAIN, 12));
+		
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.TRAILING)
@@ -74,24 +61,25 @@ public class Pannello extends JFrame implements ActionListener{
 					.addContainerGap(472, Short.MAX_VALUE)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
 						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(btnLogout, GroupLayout.PREFERRED_SIZE, 112, GroupLayout.PREFERRED_SIZE)
-							.addGap(18)
-							.addComponent(btnEsci, GroupLayout.PREFERRED_SIZE, 112, GroupLayout.PREFERRED_SIZE)
-							.addContainerGap())
+							.addComponent(lbllog)
+							.addGap(91))
 						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(frmtdtxtlog, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(frmtdtxtuser, GroupLayout.PREFERRED_SIZE, 79, GroupLayout.PREFERRED_SIZE)
-							.addGap(8))))
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(cal, GroupLayout.PREFERRED_SIZE, 258, GroupLayout.PREFERRED_SIZE)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addComponent(btnLogout, GroupLayout.PREFERRED_SIZE, 112, GroupLayout.PREFERRED_SIZE)
+									.addGap(18)
+									.addComponent(btnEsci, GroupLayout.PREFERRED_SIZE, 112, GroupLayout.PREFERRED_SIZE)))
+							.addGap(44))))
 		);
 		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_contentPane.createSequentialGroup()
 					.addContainerGap()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(frmtdtxtlog, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(frmtdtxtuser, GroupLayout.PREFERRED_SIZE, 15, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.RELATED, 483, Short.MAX_VALUE)
+					.addComponent(lbllog)
+					.addGap(98)
+					.addComponent(cal, GroupLayout.PREFERRED_SIZE, 167, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED, 218, Short.MAX_VALUE)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 						.addComponent(btnLogout, GroupLayout.PREFERRED_SIZE, 32, GroupLayout.PREFERRED_SIZE)
 						.addComponent(btnEsci, GroupLayout.PREFERRED_SIZE, 32, GroupLayout.PREFERRED_SIZE))
